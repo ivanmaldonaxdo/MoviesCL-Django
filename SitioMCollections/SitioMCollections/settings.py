0b42fce79181c745a9188f85f81d3869243c3f7c
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2022-10-31 20:12:32 -0300

    Seteo de diseño

--- SitioMCollections/SitioMCollections/settings.py
+++ SitioMCollections/SitioMCollections/settings.py
@@ -132,8 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static','admin')
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
