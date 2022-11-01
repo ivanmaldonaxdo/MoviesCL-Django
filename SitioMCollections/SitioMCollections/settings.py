dcd0efa81282eef1caf4f4e42a79c504180b79e4
ivanmaldonaxdo <ivanmaldonaxdo98@gmail.com>
2022-11-01 15:11:57 -0300

    STATICFILESDIRS CONFLICTIVOS

--- SitioMCollections/SitioMCollections/settings.py
+++ SitioMCollections/SitioMCollections/settings.py
@@ -137,10 +137,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'Peliseries/static'),
-        os.path.join(BASE_DIR, 'static')
-    ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
