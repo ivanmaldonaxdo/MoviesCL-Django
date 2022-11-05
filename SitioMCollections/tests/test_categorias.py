3ac50933bcf1e28b8bcdf9e2e6c1a4211b22f2ec
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2022-11-04 23:32:19 -0300

    Pruebas unitarias

---
+++ SitioMCollections/tests/test_categorias.py
@@ -0,0 +1,49 @@
+import pytest 
+from PeliSeries.models import Peliserie,Categoria
+
+@pytest.mark.django_db
+def test_categ_create():
+    Categoria().objects.create(1,"Podcast")
+    count = Categoria().objects.count()
+    assert count == 1
+
+# @pytest.fixture
+
+# def Categoria_1(db):
+#     return 
+
+# #DEFINICION DE FUNCIONES
+
+# # @pytest.mark.xfail
+# @pytest.mark.django_db
+# def test_peliserie_create():
+#     contenido = Peliserie.objects.create(
+#         titulo="En busca de la felicidad",
+#         autor = "Will Smith",
+#         reparto= "Will Smith ,Jaden Smith",
+#         descripcion="",
+#         genero="",
+#         year_estreno= 1998,
+#         admin=1,
+#         categoria=1,
+
+#     )
+#     assert contenido.autor == "Will Smith"
+
+# @pytest.mark.skip
+# @pytest.mark.django_db
+# def test_peliserie_create_without_autor():
+#     contenido = Peliserie.objects.create(
+#         titulo="En busca de la felicidad",
+#         autor = "Will Smith",
+#         reparto= "Will Smith ,Jaden Smith",
+#         descripcion="",
+#         genero="",
+#         year_estreno= 1998,
+#         admin=1,
+#         categoria=1,
+
+#     )
+#     assert contenido.autor == "Will Smith"
+#referencias 
+#https://www.youtube.com/watch?v=P_hXyudB8zc
--- SitioMCollections/tests/test_peliserie.py
+++ SitioMCollections/tests/test_peliserie.py
@@ -3,36 +3,22 @@ from PeliSeries.models import Peliserie
 #DEFINICION DE FUNCIONES
 
 # @pytest.mark.xfail
-@pytest.mark.skip
-@pytest.mark.django_db
-def test_peliserie_create():
-    contenido = Peliserie.objects.create(
-        titulo="En busca de la felicidad",
-        autor = "Will Smith",
-        reparto= "Will Smith ,Jaden Smith",
-        descripcion="",
-        genero="",
-        year_estreno= 1998,
-        admin=1,
-        categoria=1,
+# @pytest.mark.skip
+# @pytest.mark.django_db
+# def test_peliserie_create():
+#     contenido = Peliserie.objects.create(
+#         titulo="En busca de la felicidad",
+#         autor = "Will Smith",
+#         reparto= "Will Smith ,Jaden Smith",
+#         descripcion="",
+#         genero="",
+#         year_estreno= 1998,
+#         admin=1,
+#         categoria=1,
 
-    )
-    assert contenido.autor == "Will Smith"
+#     )
+#     assert contenido.autor == "Will Smith"
 
-@pytest.mark.skip
-@pytest.mark.django_db
-def test_peliserie_create_without_autor():
-    contenido = Peliserie.objects.create(
-        titulo="En busca de la felicidad",
-        autor = "Will Smith",
-        reparto= "Will Smith ,Jaden Smith",
-        descripcion="",
-        genero="",
-        year_estreno= 1998,
-        admin=1,
-        categoria=1,
 
-    )
-    assert contenido.autor == "Will Smith"
 #referencias 
 #https://www.youtube.com/watch?v=P_hXyudB8zc
