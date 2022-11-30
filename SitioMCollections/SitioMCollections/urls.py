75befb38817bfe8944f5dc52ea51a13602542c26
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2022-11-30 03:28:54 -0300

    Settings corregidos

--- SitioMCollections/SitioMCollections/urls.py
+++ SitioMCollections/SitioMCollections/urls.py
@@ -25,7 +25,11 @@ urlpatterns = [
     url(r'usuarios/', include ('django.contrib.auth.urls')),
     url(r'usuarios/', include ('usuarios.urls')),
     
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+# +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
 # if settings.DEBUG:
 
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
